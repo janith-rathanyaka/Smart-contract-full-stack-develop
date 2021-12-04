6cffe2ccd5c378e88c325cf54b23335e0e9b36ac
janith-rathnayaka <mnjrathnayaka97@yahoo.com>
2021-12-04 16:58:02 +0530

    update RWD, Tether, DecentralBank contacts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,17 +1,23 @@
+const Tether = artifacts.require("Tether");
+const RWD = artifacts.require("RWD");
+const DecentralBank = artifacts.require("DecentralBank");
 
-const Tether = artifacts.require('Tether')
-const RWD = artifacts.require('RWD')
-const DecentralBank = artifacts.require('DecentralBank')
+module.exports = async function(deployer, network, accounts) {
+  // Deploy Mock Tether contact
+  await deployer.deploy(Tether);
+  const tether = await Tether.deployed()
+  // Deploy RWD Contract
+  await deployer.deploy(RWD);
+  const rwd = await RWD.deployed();
 
-module.exports = async function(deployer) {
-    
-    // Deploy Mock Tether contact
-    await deployer.deploy(Tether)
+  // Deploy DecentralBank Contact
+  await deployer.deploy(DecentralBank, rwd.address, tether.address);
+  const decentralBank = await DecentralBank.deployed()
 
-    // Deploy RWD Contract 
-    await deployer.deploy(RWD)
+  // Transfer all RWD tokens to Decental Bank
+  await rwd.transfer(decentralBank.address, '1000000000000000000000000')
 
-    // Deploy DecentralBank Contact
-    await deployer.deploy(DecentralBank)
+  // Distribute 100 Tether token to investor
+  await tether.transfer(accounts[1], '100000000000000000000')
 
-}
+};
